952326b7c72201a91a1c747c061695a2d22ae559
Shareef Jalloq <sjalloq@users.noreply.github.com>
2019-05-29 22:28:31 +0100

    Emacs config : adding Dracula

--- init.el
+++ init.el
@@ -1,6 +1,21 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path "~/.emacs.d/lisp")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
 (show-paren-mode)
+
+;; ----------------------------------------------------------------------
+;; Themes
+;; ----------------------------------------------------------------------
+
+(load-theme 'dracula t)
+
 
 ;; ----------------------------------------------------------------------
 ;; Verilog Mode
@@ -85,3 +100,19 @@
 (define-key verilog-minor-mode-map (kbd "<f12>") 'verilog-uncomment-region)
 
 (define-key verilog-minor-mode-map (kbd "<f10>") 'revert-buffer)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(custom-enabled-themes (quote (wheatgrass))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
